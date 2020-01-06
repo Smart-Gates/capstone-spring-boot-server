2a15246217e91585c5aa554480dc7d71299b5788
Richard Therrien <richardgtherrien@gmail.com>
2020-01-05 22:23:56 -0500

    first commit

---
+++ src/main/java/smartgate/capstonespringboot/CapstoneSpringBootApplication.java
@@ -0,0 +1,13 @@
+package smartgate.capstonespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneSpringBootApplication.class, args);
+	}
+
+}
