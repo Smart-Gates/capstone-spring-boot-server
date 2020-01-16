f2ee6f61ce8396d18ab11b4c889081deb0db78a0
Richard Therrien <richardgtherrien@gmail.com>
2020-01-16 00:58:18 -0500

    removed boilerplate getters/setters from User model

--- src/main/java/smartgate/capstonespringboot/models/User.java
+++ src/main/java/smartgate/capstonespringboot/models/User.java
@@ -19,6 +19,8 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.NaturalId;
 
+import lombok.Data;
+@Data
 @Entity
 @Table(name = "users", uniqueConstraints = {
 	@UniqueConstraint(columnNames = {
@@ -68,51 +70,4 @@ public class User {
         this.lastName = lastName;
     }
     
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
